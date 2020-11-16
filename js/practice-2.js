09a676fd30cf76f914901e6fcfac05e333e2dd58
RuslanZahriadskyi <67427154+RuslanZahriadskyi@users.noreply.github.com>
2020-11-16 22:17:14 +0100

    best practice task 2

--- js/practice-2.js
+++ js/practice-2.js
@@ -74,6 +74,8 @@ const input = [
   { type: "beer bottle", material: "glass", secondMaterial: "paper" },
 ];
 
+console.log(recycle(input));
+
 // function recycle(array) {
 //   const PAPER = "paper";
 //   const GLASS = "glass";
@@ -122,4 +124,29 @@ const input = [
 //   return output;
 // }
 
-console.log(recycle(input));
+// ================================================================
+// Best Practices
+
+const recyclerMaterials = ["paper", "glass", "organic", "plastic"];
+
+function tet(objects) {
+  return recyclerMaterials.map((m) =>
+    objects
+      .filter((o) => o.material == m || o.secondMaterial == m)
+      .map((o) => o.type)
+  );
+}
+
+const objects = [
+  { type: "rotten apples", material: "organic" },
+  {
+    type: "out of date yogurt",
+    material: "organic",
+    secondMaterial: "plastic",
+  },
+  { type: "wine bottle", material: "glass", secondMaterial: "paper" },
+  { type: "amazon box", material: "paper" },
+  { type: "beer bottle", material: "glass", secondMaterial: "paper" },
+];
+
+console.log(tet(objects));
